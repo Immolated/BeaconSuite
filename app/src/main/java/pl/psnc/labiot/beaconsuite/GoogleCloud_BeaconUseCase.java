8cc493e24545199b7b091c0e55843dfdf718e8a6
Immolated <chodyla.m@gmail.com>
2018-10-16 17:02:19 +0200

    added some template files for further development.

---
+++ app/src/main/java/pl/psnc/labiot/beaconsuite/GoogleCloud_BeaconUseCase.java
@@ -0,0 +1,13 @@
+package pl.psnc.labiot.beaconsuite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GoogleCloud_BeaconUseCase extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google_cloud__beacon_use_case);
+    }
+}
