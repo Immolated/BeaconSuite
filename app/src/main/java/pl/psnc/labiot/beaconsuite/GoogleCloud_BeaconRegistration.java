c197a931326b6536c700e63eedc0445bad40fde9
Immolated <chodyla.m@gmail.com>
2018-10-23 16:03:26 +0200

    Added a TODO list in GoogleCloud_BeaconRegistration.java

--- app/src/main/java/pl/psnc/labiot/beaconsuite/GoogleCloud_BeaconRegistration.java
+++ app/src/main/java/pl/psnc/labiot/beaconsuite/GoogleCloud_BeaconRegistration.java
@@ -9,5 +9,15 @@ public class GoogleCloud_BeaconRegistration extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google_cloud__beacon_registration);
+
+        //TODO:
+        //1.Znaleźć beacony (Estimote SDK - skanowanie, potem zrobić z tego ListView)
+        //2.Ustawić im typ broadcastu na Eddystone-UID (Estimote SDK)
+        //3.Znaleźć beacony z ustawionym typem pakietu na Eddystone-UID (Google Proximity API + ListView)
+        //4.Udostępnić możliwość rejestracji beacona:
+        //  -zalogować się do konta google cloud
+        //  -wysłać odpowiednio spreparowanego requesta do serwera na odpowiednim adresie
+        //  -wyłapać wszystkie błędy
+        //  -zapdejtować jakoś status wyświetlanego "znalezionego" beacona na "zarejestrowany"
     }
 }
